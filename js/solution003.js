5d1362b41b6e0acf8d5ff5bb8d93ad575eb4a744
ChaPhilip <webdevphilc@gmail.com>
2022-08-25 11:22:38 -0700

    grasshopper-Summation

--- js/solution003.js
+++ js/solution003.js
@@ -8,5 +8,14 @@
 // summation(8) -> 36
 // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 
-//p get a number and get the absolute value of that number
+//p get a number value above zero and add the two numbers together
 
+
+
+var summation = function (num) {
+    let sum = 0
+    for(let i = 0; i<=num; i++) {
+        sum += i
+    }
+    return sum
+  }
