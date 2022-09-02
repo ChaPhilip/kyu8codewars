1a3e86ef65bcb90fda267ae920f101db1147e65c
ChaPhilip <webdevphilc@gmail.com>
2022-09-02 09:51:17 -0700

    smallest integer in the array

---
+++ js/solution014.js
@@ -0,0 +1,23 @@
+//Remove String Spaces
+//Simple, remove the spaces from the string, then return the resultant string.
+
+
+function noSpace(x){
+    let emptyString = ''
+for (let i = 0; i < x.length; i++){
+
+  if (x[i] === ' '){
+    emptyString += ''
+  }
+  else{
+    emptyString += x[i]
+  }
+ 
+} return emptyString
+}
+
+//method
+function noSpace(x){
+    return x.split(' ').join('')
+}
+
