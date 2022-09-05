1e57022b9a8598dd74a18a241898ca5f844c7c03
ChaPhilip <webdevphilc@gmail.com>
2022-09-05 09:23:28 -0700

    smallest integer in the array

---
+++ js/solution013.js
@@ -0,0 +1,11 @@
+
+// Find the smallest integer in the array
+//Given [34, 15, 88, 2] your solution will return 2
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+    }
+  }
+  
+  
