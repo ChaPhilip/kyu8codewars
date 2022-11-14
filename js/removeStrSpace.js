1180c3572fe10d3dffa13953fc522513060da570
philip <webdevphilc@gmail.com>
2022-11-14 10:54:12 -0800

    returningStr

---
+++ js/removeStrSpace.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+// need to split them to be individual str and than combine with join()
+
+function noSpace(x){
+    return x.split(' ').join('')
+    }
